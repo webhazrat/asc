0bd5c62c9d78d2cc9b56fdc170711f9568e5014f
webhazrat <mdhazrat445891@gmail.com>
2024-04-01 01:20:37 +0600

    Try to error fix

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,13 +1,10 @@
 "use client";
 import Title from "@/components/dashbaord/Title";
-import { eventsColumns } from "@/components/datatable/Columns";
-import { DataTable } from "@/components/datatable/DataTable";
 
 export default function Page() {
   return (
     <>
       <Title title="ইউজারস" />
-      <DataTable columns={eventsColumns()} data={[]} />
     </>
   );
 }
